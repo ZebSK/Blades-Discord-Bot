4e1f8bbe60493284253095746e3b264a32bd94bc
ZebSK <zebperry11@gmail.com>
2024-02-27 14:10:56 +0000

    FT: add views to bot on restart

--- cogs/general.py
+++ cogs/general.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+from utils import *
+
 class General(commands.Cog):
     """
     Module handling responses to events and other general functionality
@@ -17,6 +19,14 @@ class General(commands.Cog):
     async def on_ready(self):
         """ Outputs message in terminal when the bot comes online """
         print(f"{self.bot.user.name} is online.")
+
+    @commands.Cog.listener()
+    async def on_connect(self):
+        """ Re-adds views to all active clocks when bot comes back online. """
+        for clock_view in get_buttons():
+            id, segments, current_ticks, colour, title = clock_view  # unpack view for each clock
+            self.bot.add_view(ProgressView(id = id, segments = segments, current_ticks = current_ticks, colour = colour, title = title, reset = True))
+
 
 
 # Sets up the cog and adds it to the bot
